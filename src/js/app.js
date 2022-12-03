65d29ac6f1511b1d86ab4f03952070cbde0d8f1e
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-12-03 16:37:45 +0200

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
 import "../scss/app.scss";
 
-window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-});
+// This block will be executed once the page is loaded and ready
+  window.addEventListener("DOMContentLoaded", () => {
+    // This block will be executed once the page is loaded and ready
+    var hot = document.querySelectorAll(".hot")
+
+    hot.forEach(item => {
+      item.innerHTML += "🔥"
+    })
+  })
